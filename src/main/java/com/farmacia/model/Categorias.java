b2f67a242a9da9edcbed6cd180c36bb3729deca8
Lucas300 <ludaniel.sd@gmail.com>
2025-04-20 00:05:50 -0300

    adicionando as models de produtos e categorias

--- src/main/java/com/farmacia/model/Categorias.java
+++ src/main/java/com/farmacia/model/Categorias.java
@@ -1,6 +1,66 @@
 package com.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
 public class Categorias {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
 
+	@NotNull
+	String nome;
 
+	@NotNull
+	String descricao;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Produtos> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produtos> produtos) {
+		this.produtos = produtos;
+	}
+
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "categorias", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categorias")
+	List<Produtos> produtos;
 }
--- src/main/java/com/farmacia/model/Produtos.java
+++ src/main/java/com/farmacia/model/Produtos.java
@@ -1,6 +1,95 @@
 package com.farmacia.model;
 
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
 public class Produtos {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+
+	@NotNull
+	private String nome;
+	private String descricao;
+	private String laboratorio;
+	@DecimalMin(value = "0.0", inclusive = false, message = "O preço deve ser maior que zero")
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal preco;
 	
-	
+	private String foto;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categorias categorias;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getLaboratio() {
+		return laboratorio;
+	}
+
+	public void setLaboratio(String laboratio) {
+		this.laboratorio = laboratio;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categorias getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(Categorias categorias) {
+		this.categorias = categorias;
+	}
+
 }
